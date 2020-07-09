9ec4827f187874a644982d1c5a4f4a0de22fe68d
hugo <hugoandresdiazbernal@gmail.com>
2020-07-09 11:42:35 -0500

    Single Page Application start

---
+++ src/pages/Error404.js
@@ -0,0 +1,11 @@
+import view from "./Character";
+
+const Error404 = () => {
+  const view = `
+    <div class="Error404">
+      <h2>Error 404</h2>
+    </div>
+  `;
+  return view;
+}
+export default Error404;
--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,32 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
-  }
-  resolve: {
-    extensions: ['.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        }
-      }
-    ]
-  }
-  plugin: [
-    new HtmlWebpackPlugin([
-      {
-      inject: true,
-      template: './public/index.html',
-      filename: './index.html',
-      }
-    ])
-  ]
-}
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'main.js',
+	},
+	resolve: {
+		extensions: ['.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin([
+			{
+				inject: true,
+				template: './public/index.html',
+				filename: './index.html',
+			},
+		]),
+	],
+};
