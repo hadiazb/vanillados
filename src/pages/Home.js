b4a550dffa24dc71433b9f2b52fe0f822a5ba925
hugo <hugoandresdiazbernal@gmail.com>
2020-07-09 15:58:54 -0500

    Terminando pryecto

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,18 @@
-const Home = () => {
+import getData from '../utils/getData';
+
+const Home = async () => {
+  const characters = await getData();
 	const view = `
     <div class="Characters">
-      <article class="Character-item">
-        <a href="#/1/">
-          <img src="image" alt="name">
-          <h1>Name</h1>
-        </a>
-      </article>
+      ${characters.results.map(character => `
+        <article class="Character-item">
+          <a href="#/${character.id}/">
+            <img src="${character.image}" alt="${character.name}">
+            <h1>${character.name}</h1>
+          </a>
+        </article>
+      `).join('')}
+      
     </div>
   `;
 	return view;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
@@ -22,12 +23,18 @@ module.exports = {
 		],
 	},
 	plugins: [
-		new HtmlWebpackPlugin([
-			{
-				inject: true,
-				template: './public/index.html',
-				filename: './index.html',
-			},
-		]),
+		new HtmlWebpackPlugin({
+			inject: true,
+			template: './public/index.html',
+			filename: './index.html',
+		}),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: './src/styles/styles.css',
+					to: '',
+				},
+			],
+		}),
 	],
 };
